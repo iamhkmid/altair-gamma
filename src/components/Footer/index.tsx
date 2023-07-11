a448d8d11bed66a1d36d812bc684887cecca5d6f
Muhammad Luqmanul Hakim <iamhkmid@gmail.com>
2023-07-11 20:42:00 +0700

    update

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,26 @@
 import { styled } from "styled-components"
 import { hexToRgbA } from "../../utils/hexToRgbA"
+import { footerData } from "../../utils/constants/footer"
 
 const Footer = () => {
+
+  const onClickMedia = (url: string): void => {
+    window.open(url, '_blank')
+  }
+
   return (
     <FooterStyled>
       <div className="footer-left">
         <p>© 2023 Altair Gamma, by Muhammad Luqmanul Hakim</p>
       </div>
       <div className="footer-right">
-
+        <div className="media">
+          {footerData.map((item) => (
+            <div className="media-item" key={item.key} onClick={() => onClickMedia(item.url)}>
+              {item.icon}
+            </div>
+          ))}
+        </div>
       </div>
     </FooterStyled>
   )
@@ -19,22 +31,28 @@ export default Footer
 const FooterStyled = styled.div`
   display: flex;
   position: fixed;
+  justify-content: space-between;
+  align-items: center;
   bottom: 0;
   left: 0;
   width: 100%;
   z-index: 100;
-  padding: 10px 20px;
+  padding: 0 10px;
   backdrop-filter: blur(5px);
   background-color: ${({ theme }) => hexToRgbA(theme.colors.blue.L10, 0.2)};
   box-sizing: border-box;
+  @media screen and (min-width: ${({ theme }) => theme.breakpoint.md}px) {
+    padding: 0 20px;
+  }
   .footer-left {
     display: flex;
+    padding: 10px 0;
     > p {
-      font-size: 10px;
+      font-size: 9px;
       font-weight: 500;
       margin: 0;
       color: ${({ theme }) => theme.colors.text.L4};
-      @media screen and (min-width: ${({ theme }) => theme.breakpoint.sm}px) {
+      @media screen and (min-width: ${({ theme }) => theme.breakpoint.md}px) {
         font-size: 12px;
       }
       @media screen and (min-width: ${({ theme }) => theme.breakpoint.xxxl}px) {
@@ -44,5 +62,38 @@ const FooterStyled = styled.div`
   }
   .footer-right {
     display: flex;
+    .media {
+      display: flex;
+      gap: 3px;
+      @media screen and (min-width: ${({ theme }) => theme.breakpoint.md}px) {
+        gap: 8px;
+      }
+      @media screen and (min-width: ${({ theme }) => theme.breakpoint.xxxl}px) {
+        gap: 10px;
+      }
+      .media-item {
+        display: flex;
+        padding: 5px;
+        border-radius: 100%;
+        &:hover {
+          background-color: ${({ theme }) => theme.colors.primary.L2};
+        }
+        cursor: pointer;
+        transition: all ease 0.3s;
+        > svg {
+          fill: ${({ theme }) => theme.colors.text.L4};
+          width: 13px;
+          height: 13px;
+          @media screen and (min-width: ${({ theme }) => theme.breakpoint.md}px) {
+            width: 14px;
+            height: 14px;
+          }
+          @media screen and (min-width: ${({ theme }) => theme.breakpoint.xxxl}px) {
+            width: 15px;
+            height: 15px;
+          }
+        }
+      }
+    }
   }
 `
---
+++ src/utils/constants/footer.tsx
@@ -0,0 +1,21 @@
+import { ReactComponent as Linkedin } from '../../assets/icons/linkedin.svg'
+import { ReactComponent as Instagram } from '../../assets/icons/instagram.svg'
+import { ReactComponent as Github } from '../../assets/icons/github.svg'
+
+export const footerData = [
+  {
+    key: '1',
+    url: 'https://www.linkedin.com/in/iamhkmid/',
+    icon: <Linkedin />,
+  },
+  {
+    key: '2',
+    url: 'https://www.instagram.com/iamhkmid/',
+    icon: <Instagram />,
+  },
+  {
+    key: '3',
+    url: 'https://github.com/iamhkmid',
+    icon: <Github />,
+  }
+]
